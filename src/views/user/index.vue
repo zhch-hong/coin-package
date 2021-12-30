ee8f0a28b52ba863b08e7f1d90845d618ef0754b
zch <zhch_hong@163.com>
2021-12-30 09:46:47 +0800

    update[会员操作]: 会员补币=>会员补资产

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -2,7 +2,7 @@
   <div style="height: 100%" class="flex-column">
     <bc-tab :tabs="tabList" @select="handleSelect"></bc-tab>
     <userAdmin v-if="activeName === '会员提币'"></userAdmin>
-    <userCoin_add v-if="activeName === '会员补币'"></userCoin_add>
+    <userCoin_add v-if="activeName === '会员补资产'"></userCoin_add>
     <userConsume v-if="activeName === '会员消费'"></userConsume>
     <modify-data v-if="activeName === '资料修改'"></modify-data>
     <UpdatePassword v-if="activeName === '密码修改'"></UpdatePassword>
@@ -11,12 +11,14 @@
 </template>
 
 <script>
-import userAdmin from './components/userAdmin'
-import userCoin_add from './components/userCoin_add'
-import userRecord from './components/userRecord/index'
-import userConsume from './components/userConsume'
-import ModifyData from './components/modify-data'
-import UpdatePassword from './components/update-password'
+/* eslint-disable camelcase */
+
+import userAdmin from './components/userAdmin';
+import userCoin_add from './components/userCoin_add';
+import userRecord from './components/userRecord/index';
+import userConsume from './components/userConsume';
+import ModifyData from './components/modify-data';
+import UpdatePassword from './components/update-password';
 
 export default {
   name: 'index',
@@ -26,25 +28,25 @@ export default {
     userRecord,
     userConsume,
     ModifyData,
-    UpdatePassword
+    UpdatePassword,
   },
   data() {
     return {
-      activeName: '会员提币'
-    }
+      activeName: '会员提币',
+    };
   },
   computed: {
     tabList() {
-      return ['会员提币', '会员补币', '会员消费', '资料修改', '密码修改', '会员操作记录']
-    }
+      return ['会员提币', '会员补币', '会员消费', '资料修改', '密码修改', '会员操作记录'];
+    },
   },
   methods: {
     handleSelect(val) {
-      console.log(val)
-      this.activeName = val
-    }
-  }
-}
+      console.log(val);
+      this.activeName = val;
+    },
+  },
+};
 </script>
 
 <style scoped></style>
