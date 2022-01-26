9323c6b235fbbfa44a5831b5f594a780a31b5d06
zch <zhch_hong@163.com>
2022-01-26 15:16:34 +0800

    fix: 视频重复播放

--- src/views/ad-view/components/CarouselVideo.vue
+++ src/views/ad-view/components/CarouselVideo.vue
@@ -1,5 +1,5 @@
 <template>
-  <video ref="carouselVideo" width="100%" height="100%" autoplay muted loop :src="url"></video>
+  <video ref="carouselVideo" width="100%" height="100%" autoplay muted :src="url"></video>
 </template>
 <script>
 export default {
@@ -7,11 +7,27 @@ export default {
     url: String,
   },
 
+  data() {
+    return {
+      timer: null,
+    };
+  },
+
+  beforeDestroy() {
+    // 当广告资源不止一个时，那么在切换广告时，组件销毁前需要清除定时器
+    clearTimeout(this.timer);
+  },
+
   async mounted() {
     await this.$nextTick();
     const v = this.$refs.carouselVideo;
     v.addEventListener('ended', () => {
       this.$emit('complete');
+
+      // 当广告资源只有一个视频文件时，需要重复播放
+      this.timer = setTimeout(() => {
+        v.play();
+      }, 500);
     });
   },
 };
